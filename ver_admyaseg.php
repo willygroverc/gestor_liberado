deb45f92d85f5d3615a94a518106eb7fa0a64d30
Jorge <jsardon@yanapti.com>
2015-11-25 14:37:08 -0400

    Rediseñado formulario ver_admyaseg

--- ver_admyaseg.php
+++ ver_admyaseg.php
@@ -47,153 +47,153 @@ $row=mysql_fetch_array($result);
           ?>
         </div>
       </div>
+      <?php 
+      $sqlUsers="SELECT login_usr, nom_usr, apa_usr, ama_usr FROM users ORDER BY login_usr";
+      $rsUsers=mysql_query($sqlUsers);
+      while($tmpUsers=mysql_fetch_array($rsUsers)){
+        $lstUsers[$tmpUsers['login_usr']]=$tmpUsers['nom_usr']." ".$tmpUsers['apa_usr']." ".$tmpUsers['ama_usr'];
+      }
+      if ($row['Tipo']=="ADMINISTRACION DE RECURSOS HUMANOS") { ?>
+      <div class="row">
+        <div class="column">
+          <table class="u-full-width">
+            <thead>
+              <tr>
+                <th rowspan="2">N.</th>
+                <th rowspan="2">Actividad / Productos</th>
+                <th rowspan="2">Responsables</th>
+                <th rowspan="2">Cronogramas</th>
+                <th colspan="3" class="center">Cumplimiento</th>
+              </tr>
+              <tr>
+                <th class="center">Sí</th>
+                <th class="center">No</th>
+                <th class="center">Observaciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+              $sql6 = "SELECT * FROM admrhdet WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
+              $result6=mysql_query($sql6); 
+              while ($row6=mysql_fetch_array($result6)) {
+              ?>
+              <tr>
+                <td><?php echo $row6['num_det']; ?></td>
+                <td><?php echo $row6['activprod'] ?></td>
+                <td><?php echo $lstUsers[$row6['nombresp']]; ?></td>
+                <td><?php echo $row6['cronograma']; ?></td>
+                <td class="center">
+                  <?php if (($row6['cumplimiento']=="SI")){
+                    echo "&#10003;";
+                  } else { echo "&nbsp;"; } ?>
+                </td>
+                <td class="center">
+                  <?php if (($row6['cumplimiento']=="NO")){
+                    echo "&#10003;";
+                  } else { echo "&nbsp;"; } ?>
+                </td>
+                <td class="center"><?php echo $row6['observaciones']; ?></td>
+              </tr>
+              <?php } //end while ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <?php } //end if
+      else { ?>
+      <br>
+      <h6>
+        <?php if ($row['Tipo']=="ADMINISTRACION DEL ALCANCE"){echo "Alcance";}
+        else {echo "Objetivos";} ?>
+      </h6>
+      <div class="row">
+        <div class="column">
+          <table class="u-full-width">
+            <thead>
+              <tr>
+                <th>N.</th>
+                <th>Descripción</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+              $sql2 = "SELECT * FROM admyasegalcance WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
+              $result2=mysql_query($sql2); 
+              while ($row2=mysql_fetch_array($result2)) 
+              { ?>
+              <tr>
+                <td><?php echo $row2['IdAlcance']; ?></td>
+                <td><?php echo $row2['Alcance']; ?></td>
+              </tr>
+              <?php } // end while ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <br>
+      <h6>Actividades</h6>
+      <div class="row">
+        <div class="column">
+          <table class="u-full-width">
+            <thead>
+              <tr>
+                <th>N.</th>
+                <th>Actividades</th>
+                <th>Responsables</th>
+                <th>
+                  <?php if($row['Tipo']=="ADMINISTRACION DEL ALCANCE")
+                  {echo "Seguimiento";}
+                  if($row['Tipo']=="ADMINISTRACION DE LA COMUNICACION")
+                  {echo "Seguimiento";}
+                  if($row['Tipo']=="ASEGURAMIENTO DE LA CALIDAD")
+                  {echo "Métricas";} ?>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php $sql3 = "SELECT * FROM admyasegactiv WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
+              $result3=mysql_query($sql3); 
+              while ($row3=mysql_fetch_array($result3)){ ?>
+              <tr>
+                <td><?php echo $row3['IdActividad']; ?></td>
+                <td><?php echo $row3['Actividad']; ?></td>
+                <td>
+                  <?php $sql5="SELECT * FROM users WHERE login_usr='".$row3['RespActividad']."'";
+                  $result5=mysql_query($sql5);
+                  $row5=mysql_fetch_array($result5);
+                  echo @$row3['nom_usr']." ".@$row3['apa_usr']." ".@$row3['ama_usr'];
+                  echo $row5['nom_usr']." ".$row5['apa_usr']." ".$row5['ama_usr']; ?>
+                </td>
+                <td><?php echo $row3['Seguimiento']; ?></td>
+              </tr>
+              <?php } //end while ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <?php } //end else ?>
+      <br><br>
+      <div class="row">
+        <div class="column">
+          <strong>Documentación de Referencia: </strong><?php echo $row['DocuRef'];?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <strong>Documentación de Soporte: </strong><?php echo $row['DocuSoporte'];?>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="six columns">
+          <strong>Firma: </strong>
+        </div>
+        <div class="six columns">
+          <strong>Fecha: </strong>
+        </div>
+      </div>
+      <br>
     </div> <!-- end print-area -->
   </div>
-
-
-
-<br>
-
-<?php 
-$sqlUsers="SELECT login_usr, nom_usr, apa_usr, ama_usr FROM users ORDER BY login_usr";
-$rsUsers=mysql_query($sqlUsers);
-while($tmpUsers=mysql_fetch_array($rsUsers)){
-	$lstUsers[$tmpUsers['login_usr']]=$tmpUsers['nom_usr']." ".$tmpUsers['apa_usr']." ".$tmpUsers['ama_usr'];
-}
-
-if ($row['Tipo']=="ADMINISTRACION DE RECURSOS HUMANOS")
-	{
-	echo "<table width=\"85%\" border=\"1\" align=\"center\">";
-  	echo "<tr align=\"center\">";
-    echo "<td rowspan=\"2\"><strong>NUMERO</strong></td>";
-    echo "<td rowspan=\"2\"><strong>ACTIVIDAD /PRODUCTOS</strong></td>";
-    echo "<td rowspan=\"2\"><strong>RESPONSABLES</strong></td>";
-    echo "<td rowspan=\"2\"><strong>CRONOGRAMAS</strong></td>";
-    echo "<td colspan=\"2\"><strong>CUMPLIMIENTO</strong></td>";
-	echo "</tr>";
-	echo "<tr align=\"center\">"; 
-    echo "<td><strong>SI</strong></td>";
-    echo "<td><strong>NO</strong></td>";
-    echo "<td><strong>OBSERVACIONES</strong></td>";
-	echo "</tr>";
-$sql6 = "SELECT * FROM admrhdet WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
-$result6=mysql_query($sql6); 
-while ($row6=mysql_fetch_array($result6)) 
-{
-  	echo "<tr align=\"center\">";
- 	echo '<td align="center">&nbsp;'.$row6['num_det'].'</td>';
- 	echo '<td align="center">&nbsp;'.$row6['activprod'].'</td>';
- 	echo "<td align=\"center\">&nbsp;".$lstUsers[$row6['nombresp']]."</td>";
- 	echo '<td align="center">&nbsp;'.$row6['cronograma'].'</td>';
-	if ($row6['cumplimiento']=="SI")
-	{echo "<td align=\"center\"><img src=\"images/si1.gif\" border=\"1\"></td>";}
-	else
-	{echo "<td align=\"center\"><img src=\"images/no1.gif\" border=\"1\"></td>";}
-	if ($row6['cumplimiento']=="NO")
-	{echo "<td align=\"center\"><img src=\"images/si1.gif\" border=\"1\"></td>";}
-	else
-	{echo "<td align=\"center\"><img src=\"images/no1.gif\" border=\"1\"></td>";}
- 	echo '<td align="center">&nbsp;'.$row6['observaciones'].'</td>';
-	echo "</tr>";
-}
-}
-else
-{
-	echo "</table>";
-	echo "<table width=\"85%\" border=\"1\" align=\"center\">";
-  	echo "<tr align=\"center\">";
-    echo "<td colspan=\"2\"><strong><font size=\"2\" face=\"Arial, Helvetica, sans-serif\">"; 
-    if($row['Tipo']=="ADMINISTRACION DEL ALCANCE")
-	{echo "ALCANCE";}
-	else
-	{echo "OBJETIVOS";}
-	echo "</font></strong></div></td>";
-	echo "</tr>";
- echo "<tr align=\"center\">";
-    echo "<td width=\"10%\"><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>NUMERO</strong></font></td>";
-    echo "<td><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>DESCRIPCION</strong></font></td>";
-  echo "</tr>";
-$sql2 = "SELECT * FROM admyasegalcance WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
-$result2=mysql_query($sql2); 
-while ($row2=mysql_fetch_array($result2)) 
-{
-  	echo "<tr align=\"center\">";
- 	echo '<td align="center">&nbsp;'.$row2['IdAlcance'].'</td>';
- 	echo '<td align="center">&nbsp;'.$row2['Alcance'].'</td>';
-	echo "</tr>";
-}
-echo "</table>";
-echo "<br>";
-echo "<table width=\"85%\" border=\"1\" align=\"center\">";
-echo "<tr align=\"center\"> ";
-echo "<td width=\"10%\"><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>NUMERO</strong></font></td>";
-echo "<td><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>ACTIVIDADES</strong></font></td>";
-echo "<td><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>RESPONSABLES</strong></font></td>";
-echo "<td><font size=\"2\" face=\"Arial, Helvetica, sans-serif\"><strong>";
-	if($row['Tipo']=="ADMINISTRACION DEL ALCANCE")
-	{echo "SEGUIMIENTO";}
-	if($row['Tipo']=="ADMINISTRACION DE LA COMUNICACION")
-	{echo "SEGUIMIENTO";}
-	if($row['Tipo']=="ASEGURAMIENTO DE LA CALIDAD")
-	{echo "MÉTRICAS";}
-echo "</strong></font></div></td>";
-echo "</tr>";
-$sql3 = "SELECT * FROM admyasegactiv WHERE IdAdmyAseg='$idadmy' AND Tipo='$Tip'";
-$result3=mysql_query($sql3); 
-	while ($row3=mysql_fetch_array($result3)){
-		echo "<tr align=\"center\">";
-		echo '<td>&nbsp;'.$row3['IdActividad'].'</td>';
-		echo '<td>&nbsp;'.$row3['Actividad'].'</td>';
-		$sql5="SELECT * FROM users WHERE login_usr='".$row3['RespActividad']."'";
-		$result5=mysql_query($sql5);
-		$row5=mysql_fetch_array($result5);
-		echo @$row3['nom_usr']." ".@$row3['apa_usr']." ".@$row3['ama_usr'];
-		echo '<td>&nbsp;'.$row5['nom_usr'].' '.$row5['apa_usr'].' '.$row5['ama_usr'].'</td>';
-		echo '<td>&nbsp;'.$row3['Seguimiento'].'</td>';
-		echo "</tr>";
-	}
-}
-?>
-
-</table>
-<br>
-<table  width="564" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="237"><font size="2" face="Arial, Helvetica, sans-serif"><strong>DOCUMENTACION DE REFERENCIA : </strong></font></td>
-    <td width="327"><?php echo $row['DocuRef'];?></td>
-  </tr>
-  <tr> 
-    <td height="2"></td>
-    <td bgcolor="#000000"></td>
-  </tr>
-</table>
-<br>
-<table  width="560" height="24" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr> 
-    <td width="226"><font size="2" face="Arial, Helvetica, sans-serif"><strong>DOCUMENTACION DE SOPORTE:</strong></font></td>
-    <td width="340"><?php echo $row['DocuSoporte'];?></td>
-  </tr>
-  <tr> 
-    <td height="2"></td>
-    <td bgcolor="#000000"></td>
-  </tr>
-</table>
-<br>
-<table  width="563" height="25" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="61"><font size="2" face="Arial, Helvetica, sans-serif"><strong>FIRMA:</strong></font></td>
-    <td width="283"> </td>
-    <td width="64"><font size="2" face="Arial, Helvetica, sans-serif"><strong>FECHA:</strong></font></td>
-    <td width="155"><?php echo $row['FechaAdAs'];?></td>
-
-  </tr>
-  <tr> 
-    <td height="2"></td>
-    <td bgcolor="#000000"></td>
-    <td height="2"></td>
-    <td bgcolor="#000000"></td>
-  </tr>
-</table>
-<p></p>
 </body>
 </html>
